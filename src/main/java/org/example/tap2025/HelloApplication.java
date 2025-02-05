e2e2d0cebe6dfe6b2f0ec7886fce4ec246c137fd
Pau Escalante <pauesc122@gmail.com>
2025-02-05 12:43:05 -0600

    menu agregado al proyecto

--- src/main/java/org/example/tap2025/HelloApplication.java
+++ src/main/java/org/example/tap2025/HelloApplication.java
@@ -2,22 +2,47 @@ package org.example.tap2025;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private VBox vBox;
+    private MenuBar mnbPrincipal;
+    private Menu menCompetencia1, MenCompetencia2;
+    private MenuItem mitCalculadora;
+    private Scene escena;
+
+    void CrearUi(){
+        mitCalculadora = new MenuItem("calculadora");
+        menCompetencia1 = new Menu("competencia 1");
+        menCompetencia1.getItems().addAll(mitCalculadora);
+
+        mnbPrincipal = new MenuBar();
+        mnbPrincipal.getMenus().addAll(menCompetencia1);
+
+    }
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        vBox = new VBox(); //apilar los botones en un objeto en vertical
+
+        stage.setTitle("Hola mundo de eventos :)");
+        stage.setTitle("Holi");
+        stage.setScene(new Scene(vBox,200,200));
         stage.show();
+        stage.setMaximized(true);
     }
 
-    public static void main(String[] args) {
-        launch();
+    public static void main(String[] args) {launch();
+
+    }
+    void clicEvent(){
+        System.out.println("Evento con metodo");
     }
 }
