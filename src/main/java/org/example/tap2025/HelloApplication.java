f5e53d665ad8dc24f8c99833be99030580377890
Pau Escalante <pauesc122@gmail.com>
2025-02-07 12:43:10 -0600

    Calculadora

--- src/main/java/org/example/tap2025/HelloApplication.java
+++ src/main/java/org/example/tap2025/HelloApplication.java
@@ -5,44 +5,50 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.example.tap2025.vistas.Calculadora;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     private VBox vBox;
     private MenuBar mnbPrincipal;
-    private Menu menCompetencia1, MenCompetencia2;
+    private Menu menCompetencia1;
     private MenuItem mitCalculadora;
     private Scene escena;
 
-    void CrearUi(){
-        mitCalculadora = new MenuItem("calculadora");
-        menCompetencia1 = new Menu("competencia 1");
+    void CrearUi() {
+        mitCalculadora = new MenuItem("Calculadora");
+        mitCalculadora.setOnAction(actionEvent -> new Calculadora());
+        menCompetencia1 = new Menu("Competencia 1");
         menCompetencia1.getItems().addAll(mitCalculadora);
 
         mnbPrincipal = new MenuBar();
         mnbPrincipal.getMenus().addAll(menCompetencia1);
-
+        vBox = new VBox(mnbPrincipal);
     }
+
     @Override
     public void start(Stage stage) throws IOException {
-        vBox = new VBox(); //apilar los botones en un objeto en vertical
-
-        stage.setTitle("Hola mundo de eventos :)");
-        stage.setTitle("Holi");
-        stage.setScene(new Scene(vBox,200,200));
+        CrearUi();
+        stage.setTitle("Mundo de eventos");
+        stage.setScene(new Scene(vBox, 200, 200));
         stage.show();
         stage.setMaximized(true);
-    }
 
-    public static void main(String[] args) {launch();
 
     }
-    void clicEvent(){
-        System.out.println("Evento con metodo");
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    void clicEvent() {
+        System.out.println("Evento con método");
     }
 }
---
+++ src/main/java/org/example/tap2025/vistas/Calculadora.java
@@ -0,0 +1,53 @@
+package org.example.tap2025.vistas;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Calculadora extends Stage {
+    private Scene escena;
+    private TextField txtDisplay;
+    private VBox vBox;
+    private GridPane gdpTeclado;
+    private Button[][] arBtnTeclado;
+    String strTeclas[] = {"7", "8","9","*", "4", "5", "6", "/", "1", "2", "3", "+", "=", "0",".", "-" };
+
+    public void CrearUi(){
+        CrearKeyboard();
+        txtDisplay = new TextField("0");
+        vBox = new VBox(txtDisplay, gdpTeclado);
+        txtDisplay.setEditable(false);
+        txtDisplay.setAlignment(Pos.BASELINE_RIGHT);
+        vBox.setSpacing(10);
+        //vBox.setPadding(new Insets(topRightBottomLeft: 10));
+        escena = new Scene(vBox, 200,200);
+
+    }
+    public void CrearKeyboard() {
+        arBtnTeclado = new Button[4][4];
+        gdpTeclado = new GridPane();
+        gdpTeclado.setHgap(5);
+        gdpTeclado.setVgap(5);
+        int pos= 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+
+                arBtnTeclado[i][j] = new Button(strTeclas[pos]);
+                arBtnTeclado[i][j].setPrefSize(45 , 45);
+                gdpTeclado.add(arBtnTeclado[i][j], j, i);
+                pos++;
+            }
+        }
+    }
+    public Calculadora(){
+        CrearUi();
+        this.setScene(escena);
+        this.setTitle("Calculadora");
+        this.show();
+    }
+
+}
